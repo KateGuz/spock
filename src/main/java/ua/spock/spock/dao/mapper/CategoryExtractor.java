aa12b18ac53c358c5957f897862e2c5f5425c8d9
DanyloZ <d.zrazhevskiy@gmail.com>
2016-11-10 20:44:32 +0200

    refactored

--- src/main/java/ua/spock/spock/dao/mapper/CategoryExtractor.java
+++ src/main/java/ua/spock/spock/dao/mapper/CategoryExtractor.java
@@ -18,19 +18,11 @@ public class CategoryExtractor implements ResultSetExtractor<List<Category>> {
         List<Category> allCategories = new ArrayList<>();
         Category parent = null;
         while (rs.next()) {
-            if (rs.getInt("parentId") == 0) {
-                parent = new Category();
-                parent.setId(rs.getInt("id"));
-                parent.setName(rs.getString("name"));
-                allCategories.add(parent);
-            } else {
-                if (parent.getId() != rs.getInt("parentId")) {
-                    for (Category category : allCategories) {
-                        if (category.getId() == rs.getInt("parentId")) {
-                            parent = category;
-                            break;
-                        }
-                    }
+                if (parent == null || parent.getId() != rs.getInt("parentId")) {
+                    parent = new Category();
+                    parent.setId(rs.getInt("parentId"));
+                    parent.setName(rs.getString("parentName"));
+                    allCategories.add(parent);
                 }
                 Category child = new Category();
                 child.setId(rs.getInt("id"));
@@ -38,7 +30,7 @@ public class CategoryExtractor implements ResultSetExtractor<List<Category>> {
                 child.setParent(parent);
                 parent.getChildren().add(child);
             }
-        }
+
         return allCategories;
     }
 }

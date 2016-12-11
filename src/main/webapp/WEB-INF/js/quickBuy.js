d24cef150651807a07630dfe7d777485a97acd47
DanyloZ <d.zrazhevskiy@gmail.com>
2016-12-11 02:24:48 +0200

    confirmation dialog box was added to quick buy function

--- src/main/webapp/WEB-INF/js/quickBuy.js
+++ src/main/webapp/WEB-INF/js/quickBuy.js
@@ -1,14 +1,16 @@
 function quickBuy() {
     var lotId = $("#quickBuy").val();
-    $.ajax({
-        url: '/lot/'+lotId+'/quickBuy',
-        type: 'POST',
-        success: function () {
-            alert("Лот куплен!");
-            window.location.href = "http://localhost:8080/";
-        },
-        error: function () {
-            alert("что-то пошло нетак :(");
-        }
-    })
+    if (confirm("Вы уверены, что хотите купить лот?") == true) {
+        $.ajax({
+            url: '/lot/' + lotId + '/quickBuy',
+            type: 'POST',
+            success: function () {
+                alert("Лот куплен!");
+                window.location.href = "http://localhost:8080/";
+            },
+            error: function () {
+                alert("что-то пошло нетак :(");
+            }
+        })
+    }
 }

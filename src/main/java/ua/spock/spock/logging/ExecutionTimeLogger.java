1250e083db152060e09798a9c2ebabf237914a49
dimon <dimon>
2017-03-02 13:27:45 +0200

    fixed class name in log

--- src/main/java/ua/spock/spock/logging/ExecutionTimeLogger.java
+++ src/main/java/ua/spock/spock/logging/ExecutionTimeLogger.java
@@ -6,11 +6,11 @@ import org.slf4j.LoggerFactory;
 
 public class ExecutionTimeLogger {
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-        Logger logger = LoggerFactory.getLogger(joinPoint.getClass());
+        Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
         String className = joinPoint.getTarget().getClass().getSimpleName();
         String methodName = joinPoint.getSignature().getName();
         long start = System.currentTimeMillis();
-        logger.info( "{}.{}() processing started", className, methodName);
+        logger.info("{}.{}() processing started", className, methodName);
 
         Object out = joinPoint.proceed();
 
